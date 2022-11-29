76557b50da3d21bfd022ee921be336bce44f2235
kannolimahesh <45396209+kannolimahesh@users.noreply.github.com>
2022-11-29 14:12:19 +0530

    Add files via upload

---
+++ Task_LIST.php
@@ -0,0 +1,43 @@
+
+
+User task management system
+
+Registration
+
+Name -
+
+Email - It should be unique and email validation rule
+
+password - both password should same
+
+confirm password-> both password should same
+
+profile pic
+
+Date of birth -> It should be more than 18 years
+
+
+Login
+
+Email
+
+password
+
+
+Create tods task
+
+Name
+
+Description
+
+start date
+
+end date
+
+task status pending/complted
+
+
+Edit task
+
+
+List my tasks (Pending/completed)
